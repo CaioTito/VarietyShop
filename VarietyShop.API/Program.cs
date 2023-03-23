ad10ac02f50630a63a8b079b91803f825eafcd1e
Leandro Menezes <leandromenezes1@gmail.com>
2023-03-23 10:59:55 -0300

    feat: add product handler, repository and Program configuration

--- VarietyShop.API/Program.cs
+++ VarietyShop.API/Program.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
