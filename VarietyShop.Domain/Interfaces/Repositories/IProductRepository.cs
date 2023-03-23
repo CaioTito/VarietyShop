28b6caa3080b00b95a2ce02d0aa72312d88a2573
Leandro Menezes <leandromenezes1@gmail.com>
2023-03-23 10:53:11 -0300

    feat: add product handler, repository and Program configuration

---
+++ VarietyShop.Domain/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using VarietyShop.Domain.Models;
+
+namespace VarietyShop.Domain.Interfaces.Repositories;
+
+public interface IProductRepository
+{
+    Task<List<Product>> GetAllAsync();
+    Task<Product> GetByIdAsync(int id);
+    Task<Product> AddAsync(Product product);
+    Task<bool> UpdateAsync(Product product, int id);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ VarietyShop.Infra/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using VarietyShop.Domain.Interfaces.Repositories;
+using VarietyShop.Domain.Models;
+using VarietyShop.Infra.Persistence.Data;
+
+namespace VarietyShop.Infra.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly VarietyShopDataContext _dbContext;
+
+    public ProductRepository(VarietyShopDataContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<List<Product>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<Product> AddAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Product product, int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
